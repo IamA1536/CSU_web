f2b64191e8e7a654f7d5fd1321c29cf20002cf13
IamA <1694522669@QQ.COM>
2018-12-16 11:46:52 +0800

    Fix filter

--- src/com/a/JPetStore/web/filter/LogAddCartFilter.java
+++ src/com/a/JPetStore/web/filter/LogAddCartFilter.java
@@ -1,7 +1,6 @@
 package com.a.JPetStore.web.filter;
 
 import com.a.JPetStore.domain.account.Account;
-import com.a.JPetStore.domain.carts.Cart;
 import com.a.JPetStore.domain.object.Item;
 import com.a.JPetStore.serivce.CatalogSerivce;
 import com.a.JPetStore.serivce.LogSerive;
@@ -17,11 +16,11 @@ import java.io.IOException;
  */
 public class LogAddCartFilter implements Filter {
 
-    private static final String V_CART = "/WEB-INF/jsp/cart/Cart.jsp";
-    private static final String ERROR = "/WEB-INF/jsp/common/Error.jsp";
+//    private static final String V_CART = "/WEB-INF/jsp/cart/Cart.jsp";
+//    private static final String ERROR = "/WEB-INF/jsp/common/Error.jsp";
 
     private String workingItemId;
-    private Cart cart;
+//    private Cart cart;
 
     private CatalogSerivce catalogSerivce;
 
@@ -41,46 +40,47 @@ public class LogAddCartFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
         workingItemId = request.getParameter("workingItemId");
-        HttpSession session = ((HttpServletRequest)request).getSession();
-        cart = (Cart) session.getAttribute("cart");
+        HttpSession session = ((HttpServletRequest) request).getSession();
+//        cart = (Cart) session.getAttribute("cart");
         account = (Account) session.getAttribute("account");
-        session.setAttribute("account", account);
-        if (account == null) {
-            String message = "Please sign in first!";
-            session.setAttribute("message", message);
-            request.getRequestDispatcher(ERROR).forward(request, response);
-            return;
-        }
-        if (cart == null)
-            cart = new Cart();
-        if (cart.containsItemId(workingItemId)) {
-            cart.incrementQuantityByItemId(workingItemId);
-            session.setAttribute("cart", cart);
+//        session.setAttribute("account", account);
+//        if (account == null) {
+//            String message = "Please sign in first!";
+//            session.setAttribute("message", message);
+//            request.getRequestDispatcher(ERROR).forward(request, response);
+//            return;
+//        }
+//        if (cart == null)
+//            cart = new Cart();
+//        if (cart.containsItemId(workingItemId)) {
+//            cart.incrementQuantityByItemId(workingItemId);
+//            session.setAttribute("cart", cart);
+//            LogSerive logSerive = new LogSerive();
+//            try {
+//                Item item = catalogSerivce.getItem(workingItemId);
+//                String str = account.getUsername() + "adds" + item.getItemId() + "to cart";
+//                logSerive.InsertLog(account, str);
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//            request.getRequestDispatcher(V_CART).forward(request, response);
+//        } else {
+        catalogSerivce = new CatalogSerivce();
+        try {
+//                boolean isInStock = catalogSerivce.isItemInStock(workingItemId);
+            Item item = catalogSerivce.getItem(workingItemId);
+//                cart.addItem(item, isInStock);
+//                session.setAttribute("cart", cart);
+
             LogSerive logSerive = new LogSerive();
-            try {
-                Item item = catalogSerivce.getItem(workingItemId);
-                String str = account.getUsername() + "adds" + item.getItemId() + "to cart";
-                logSerive.InsertLog(account, str);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            request.getRequestDispatcher(V_CART).forward(request, response);
-        } else {
-            catalogSerivce = new CatalogSerivce();
-            try {
-                boolean isInStock = catalogSerivce.isItemInStock(workingItemId);
-                Item item = catalogSerivce.getItem(workingItemId);
-                cart.addItem(item, isInStock);
-                session.setAttribute("cart", cart);
+            String str = "Adds " + item.getItemId() + " to cart";
+            logSerive.InsertLog(account, str);
 
-                LogSerive logSerive = new LogSerive();
-                String str = "Adds " + item.getItemId() + " to cart";
-                logSerive.InsertLog(account, str);
-
-                request.getRequestDispatcher(V_CART).forward(request, response);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+            filterChain.doFilter(request, response);
+//                request.getRequestDispatcher(V_CART).forward(request, response);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+//    }
     }
 }
--- src/com/a/JPetStore/web/filter/LogVItemFilter.java
+++ src/com/a/JPetStore/web/filter/LogVItemFilter.java
@@ -3,7 +3,6 @@ package com.a.JPetStore.web.filter;
 
 import com.a.JPetStore.domain.account.Account;
 import com.a.JPetStore.domain.object.Item;
-import com.a.JPetStore.domain.object.Product;
 import com.a.JPetStore.serivce.CatalogSerivce;
 import com.a.JPetStore.serivce.LogSerive;
 
@@ -19,7 +18,7 @@ import java.io.IOException;
 public class LogVItemFilter implements Filter {
     private static final String V_ITEM = "/WEB-INF/jsp/catalog/Item.jsp";
     private String itemId;
-    private String productId;
+    //    private String productId;
     private Account account;
 
     @Override
@@ -36,24 +35,25 @@ public class LogVItemFilter implements Filter {
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
         HttpSession session = ((HttpServletRequest) request).getSession();
         itemId = request.getParameter("itemId");
-        productId = request.getParameter("productId");
+//        productId = request.getParameter("productId");
         CatalogSerivce serivce = new CatalogSerivce();
 
         try {
             Item item = serivce.getItem(itemId);
-            Product product = serivce.getProduct(productId);
+//            Product product = serivce.getProduct(productId);
 
             session.setAttribute("item", item);
-            session.setAttribute("product", product);
+//            session.setAttribute("product", product);
 
             account = (Account) session.getAttribute("account");
-            session.setAttribute("account", account);
+//            session.setAttribute("account", account);
 
             LogSerive logSerive = new LogSerive();
             String str = "Views " + item.getItemId();
             logSerive.InsertLog(account, str);
 
-            request.getRequestDispatcher(V_ITEM).forward(request, response);
+            filterChain.doFilter(request, response);
+//            request.getRequestDispatcher(V_ITEM).forward(request, response);
 
         } catch (Exception e) {
             e.printStackTrace();
